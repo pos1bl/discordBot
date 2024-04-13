196b97b14868a723e60c604a056bcd9703fca9ff
pos1bl <90213755+pos1bl@users.noreply.github.com>
2024-04-13 18:38:29 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+description = '''An example bot to respond with "OK BOSS" to every user message.'''
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:  # Ігноруємо повідомлення від інших ботів
+        return
+    
+    await message.channel.send("OK BOSS")  # Відповідаємо на кожне повідомлення словом "OK BOSS"
+
+
+bot.run(token)
